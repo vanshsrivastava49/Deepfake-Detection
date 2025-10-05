904e57d02063c4e02e05aaad96de93e288355467
vanshsrivastava49 <srivastavavansh11@gmail.com>
2025-10-05 20:57:56 +0530

    accuracy bhad gya hehehehehe

---
+++ test.py
@@ -0,0 +1,7 @@
+import torch
+
+print("PyTorch version:", torch.__version__)
+print("Is CUDA available:", torch.cuda.is_available())
+print("CUDA device count:", torch.cuda.device_count())
+if torch.cuda.is_available():
+    print("CUDA device name:", torch.cuda.get_device_name(0))
